3572edad773ef36f6cbde046887cac93a6dd50d7
Gui5002 <162457429+Gui5002@users.noreply.github.com>
2026-09-15 11:05:59 -0300

    Add files via upload

---
+++ eva_course_grid/lang/en/block_eva_course_grid.php
@@ -0,0 +1,25 @@
+<?php
+
+$string['eva_course_grid:addinstance'] = 'Add a eva featured courses block';
+$string['eva_course_grid:myaddinstance'] = 'Add a eva featured courses block to my moodle';
+$string['pluginname'] = '[eva] Featured courses masonry';
+$string['editlink'] = '<a href="{$a}">Click here to edit or add featured courses</a>';
+$string['editpagedesc'] = 'Editing the list of featured courses';
+$string['eva_course_grid'] = 'eva featured courses';
+$string['addfeaturedcourse'] = 'Add an existing course to featured list';
+$string['courseid'] = 'Course';
+$string['editpagetitle'] = 'Featured courses - editing';
+$string['featuredcourse'] = 'Featured course: {$a}';
+$string['doadd'] = 'Check to add new featured course';
+$string['missingcourseid'] = 'You must select a course.';
+$string['sortorder'] = 'Sort order';
+$string['missingsortorder'] = 'You must set a sort order.';
+$string['deletelink'] = '<a href="{$a}">Delete</a>';
+$string['delete_featuredcourse'] = 'Delete featured course';
+$string['confirmdelete'] = 'Are you sure you want to delete this course from the list of featured courses?';
+$string['config_title'] = 'Title';
+$string['config_subtitle'] = 'Subtitle';
+$string['config_button_text'] = 'Button text';
+$string['config_button_link'] = 'Button link';
+$string['config_hover_text'] = 'Hover text';
+$string['config_hover_accent'] = 'Hover accent';
---
+++ eva_course_grid/version.php
@@ -0,0 +1,28 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * ${PLUGINNAME} file description here.
+ * @package    ${PLUGINNAME}
+ * @copyright  2024 ESAGU
+ * @author     xangay® <xangay.gmail.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+defined('MOODLE_INTERNAL') || die();
+$plugin->component = 'block_eva_course_grid';  // Full name of the plugin (used for diagnostics).
+$plugin->version = 202506261210;  // The current plugin version (Date: YYYYMMDDXX).
+$plugin->requires = 2021051718;
+$plugin->incompatible = 400;
